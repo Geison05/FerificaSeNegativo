b45c5a822f7cc2cdd929b9f0bed47fb93e0e1a14
Geison Amorim <geisonam43@gmail.com>
2022-04-12 09:23:19 -0300

    Add project files.

---
+++ FerificaSeNegativo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            //Variavel 
+
+            int A ;
+
+            //Entrada
+
+            A = int.Parse(Console.ReadLine());
+
+            //Processamento / Saida
+
+            if(A < 0)
+                Console.WriteLine("NEGATIVO");
+            else
+                Console.WriteLine("NAO NEGATIVO");
+        }
+    }
+}
